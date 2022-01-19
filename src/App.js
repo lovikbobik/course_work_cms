fe61665c4827cd51c3251e43953f5b81d0ea2a45
pluxery <84563687+pluxery@users.noreply.github.com>
2022-01-20 04:13:00 +1000

    create header and sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
+import Main from "./Main/Main";
+import Layout from "./Layout/Layout";
+import Category from "./Category/Category";
+import Product from "./Product/Product";
+import waterProducts from './Data/waterProducts'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <Router>
+              <Routes>
+                  <Route path="/" exact element={<Layout children={<Main/>}/>}/>
+                  <Route path="/drinks" exact element={<Layout children={<Category/>}/>}/>
+                  <Route path="/drinks/product" exact element={<Layout children={<Product product={waterProducts[0]}/>}/>}/>
+              </Routes>
+          </Router>
+      </>
+
   );
 }
 
