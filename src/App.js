14216d2837bd802b86bad2344b69960acb7b44d2
lovikbobik <afanaseva.sd@students.dvfu.ru>
2022-01-20 16:22:52 +0300

    add map

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Main from "./Main/Main";
 import Layout from "./Layout/Layout";
 import Category from "./Category/Category";
 import Product from "./Product/Product";
-import waterProducts from './Data/waterProducts'
+import waterProducts from './Data/waterProducts';
+import MyMap from './Information/MyMap'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
                   <Route path="/" exact element={<Layout children={<Main/>}/>}/>
                   <Route path="/drinks" exact element={<Layout children={<Category/>}/>}/>
                   <Route path="/drinks/product" exact element={<Layout children={<Product product={waterProducts[0]}/>}/>}/>
+                  <Route path="/info" exact element={<Layout children={<MyMap/>}/> }/>
               </Routes>
           </Router>
       </>
