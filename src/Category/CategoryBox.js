13ace2ae74f101f16a1fa1b1ab2a356a96fd298f
lovikbobik <afanaseva.sd@students.dvfu.ru>
2022-01-20 17:32:04 +0300

    some request

--- src/Category/CategoryBox.js
+++ src/Category/CategoryBox.js
@@ -1,16 +1,22 @@
 import React from "react";
 import './CategoryBox.css'
-function CategoryBox({image, category}) {
+import {NavLink} from "react-router-dom";
+
+function CategoryBox({image, category, link = '/'}) {
     return (
 
         <div className={'categoryBox_content'}>
-            <div className={'categoryBox__image'}>
-                <img className={'categoryBox__img-size'} src={image} alt={'image not found'}/>
-            </div>
 
-            <div className={'categoryBox__name'}>
-                <p>{category}</p>
-            </div>
+                <div className={'categoryBox__image'}>
+                    <img className={'categoryBox__img-size'} src={image} alt={'image not found'}/>
+                </div>
+
+                <div className={'categoryBox__name'}>
+                    <NavLink to={link} className={'sidebar__text-decoration'}>
+                    <p>{category}</p>
+                    </NavLink>
+                </div>
+
         </div>
 
     );
