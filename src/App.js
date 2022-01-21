a22f1a231e61ddada47500fd59b2c2d7ef82e0ab
lovikbobik <afanaseva.sd@students.dvfu.ru>
2022-01-21 07:52:36 +0300

    add basket(Semen)

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ import drinksHot from "./Data/drinksHot";
 import MyMap from "./Information/MyMap";
 import Contacts from "./Information/Contacts";
 import Quality from "./Information/Quality";
-import Help from "./Information/Help";
+import Help from "./Information/Help"
+import Basket from "./Basket/Basket";
 
 function App() {
     return (
@@ -26,8 +27,9 @@ function App() {
                 <Routes>
                     <Route path="/map" exact element={<Layout children={<MyMap/>}/>}/>
                     <Route path="/contacts"exact element={<Layout children={<Contacts/>}/>}/>
-                    <Route path="/help" exact element={<Layout children={<Help/>}/>}/>
                     <Route path="/quality"exact element={<Layout children={<Quality/>}/>}/>
+                    <Route path="/help"exact element={<Layout children={<Help/>}/>}/>
+                    <Route path="/basket"exact element={<Layout children={<Basket/>}/>}/>
 
                     <Route path="/" exact element={<Layout children={<Main pizza={pizza}/>}/>}/>
                     <Route path="/drinks" exact element={<Layout children={<Category category={categoryWater}/>}/>}/>
