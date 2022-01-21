04234703ab86aaac474acdaaa5282351216e582f
lovikbobik <afanaseva.sd@students.dvfu.ru>
2022-01-21 05:07:49 +0300

    delete

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -7,34 +7,33 @@ import React from "react";
 
 function Header() {
     return (
-        <div className={'header__container'}>
-            <div className={'header__title'}>
-                <h2>WWW.DVFU.RU</h2>
+        <div className={'header'}>
+            <div className={'header__container'}>
+                <div className={'header__title'}>
+                    <h2>WWW.DVFU.RU</h2>
+                </div>
+
+                <div className={'header__menu'}>
+                    <div className={'header__logo-fefu'}>
+                        <img src={logo} alt="logo" width="100" height="80"/>
+                    </div>
+                    <div className={'header__info'}>
+                        <p className={'blue__text'}>Контакты</p>
+                        <p className={'blue__text'}>Помощь</p>
+                        <NavLink to={'/info'} className={'blue__text'}>
+                            <p>Наш адрес</p>
+                        </NavLink>
+                        <p className={'blue__text'}>Качество</p>
+
+                    </div>
+                    <div className={'header__profile'}>
+                        <p><LocalGroceryStoreOutlinedIcon/></p>
+                        <p>User</p>
+                        <p><SettingsOutlinedIcon/></p>
+                    </div>
+                </div>
             </div>
-
-            <div className={'header__menu'}>
-                <div className={'header__logo-fefu'}>
-                    <img src={logo} alt="logo"  width="100" height="80" />
-                </div>
-                <div className={'header__info'}>
-                    <p className={'blue__text'}>Контакты</p>
-                    <p className={'blue__text'}>Помощь</p>
-                    <NavLink to={'/info'} className={'blue__text'} >
-                        <p>Наш адрес</p>
-                    </NavLink>
-                    <p className={'blue__text'}>Качество</p>
-
-                </div>
-                <div className={'header__profile'}>
-                    <p><LocalGroceryStoreOutlinedIcon/></p>
-                    <p>User</p>
-                    <p><SettingsOutlinedIcon/></p>
-                </div>
-            </div>
-
-
         </div>
-
 
     )
         ;
